a3d4c4752667acee3f1a1dacb215e394cdfdd4e4
candisehenry <36269817+candisehenry@users.noreply.github.com>
2021-01-25 15:55:33 -0500

    Update d_setup_guatemala.py

--- d_setup_guatemala.py
+++ d_setup_guatemala.py
@@ -17,11 +17,6 @@ def d_setup(run_d_setup, flow_suffix, temp_suffix, wind_multiplier):
     from b_calc_enedem import calc_energydemand_tif
     from c_interp_solar import interp_solar
     from c_infrastructure import get_infrastructure
-#   from b_calc_windpower import calc_wind
-#   from b_calc_windpower import calc_wind_tif
-#   from b_calc_enedem import calc_energydemand_csv
-#   from c_interp_wind import interp_wind_csv
-#   from c_interp_enedem import interp_enedem
 
     # Size of grid cells
     step = 1/120 # step size we want
@@ -71,21 +66,13 @@ def d_setup(run_d_setup, flow_suffix, temp_suffix, wind_multiplier):
                             coord_from = coord_pop, coord_to = coord)
         
         
-        
         # Run hydro calculations and interpolations (should already be in correct step size)
         get_hydrodata(folder = 'Guatemala', catcharea_infile = 'guatemala_catcharea.tif', 
                       slope_infile = 'guatemala_slope.tif', flow_infile = 'guatemala_' + flow_suffix + '_flow.tif', 
                       catcharea_outfile = 'catcharea.npy', slope_outfile = 'slope.npy', 
                       flow_outfile = 'flow.npy')
         
-        # # Run energy demand calculations for hydro
-        # calc_energydemand_tif(folder = 'Guatemala', infile = 'hogares_offgrid_munis.tif', 
-        #                     outfile = 'guatemala_demand_interp_survey.npy', 
-        #                     step_from = step_pop, step_to = step_hyd, 
-        #                     coord_from = coord_pop, coord_to = coord_hyd)
-
-
-
+        
         # Run infrastructure set up
         get_infrastructure(folder = ['Guatemala', 'Guatemala/LandCover/'], 
                            infile = ['urbanareas_new.tif'], 
@@ -99,38 +86,4 @@ def d_setup(run_d_setup, flow_suffix, temp_suffix, wind_multiplier):
                            step_from = [step_trans], step_to = step_hyd,
                            coord_from = [coord_trans], coord_to = coord_hyd)
 
-#       calc_wind(folder = 'Guatemala', infile = 'POWER_Regional_Interannual_199001_201912_c2f60e61.csv', 
-#                 outfile1 = 'guatemala_wind_power.txt', outfile2 = 'guatemala_wind_cf.txt')
-#       interp_wind_csv(folder = 'Guatemala', infile = 'guatemala_wind_power.txt', 
-#                   outfile = 'guatemala_wind_interp.npy', step = step, coord = coord)
-#       interp_wind_csv(folder = 'Guatemala', infile = 'guatemala_wind_cf.txt', 
-#                  outfile = 'guatemala_wcf_interp.npy', step = step, coord = coord)
-#
-#       # Run wind calculations (interpolated )
-#       calc_wind_tif(folder = 'Guatemala', infile_wndspd = 'GlobalAtlasWind_GTM_windspeed_50m.tif', 
-#                  infile_pwrdns = 'GlobalAtlasWind_GTM_powerdens_50m.tif',
-#                  outfile1 = 'guatemala_wind_interp.npy', outfile2 = 'guatemala_wcf_interp.npy', 
-#                  step_from = step_wind, step_to = step, 
-#                  coord_from = coord_win, coord_to = coord,
-#                  variability = wind_multiplier)
-#
-#       calc_energydemand_csv(folder = 'Guatemala', infile = 'popdat.csv', 
-#                          outfile = 'guatemala_demand.txt')
-#       interp_enedem(folder = 'Guatemala', infile = 'guatemala_demand.txt', 
-#                          outfile = 'guatemala_demand_interp.npy', 
-#                          step = step, coord = coord)
-#
-#       # Run energy demand calculations and interpolations using high-resolution tiff file
-#       calc_energydemand_npy(folder = 'Guatemala', infile = 'gtm_ppp_2020_1km_EDIT.tif', 
-#                          outfile = 'guatemala_demand_interp_gtm.npy', 
-#                          step_from = step_pop, step_to = step, 
-#                          coord_from = coord_pop, coord_to = coord)
-#
-#       calc_energydemand_npy(folder = 'Guatemala', infile = 'gtm_ppp_2020_1km_EDIT.tif', 
-#                          outfile = 'guatemala_demand_hydro_interp_gtm.npy', 
-#                          step_from = step_pop, step_to = step, 
-#                          coord_from = coord_pop, coord_to = coord_hyd)
-
-
     return step_hyd, coord, coord_hyd
-    # return step, coord, coord_hyd
